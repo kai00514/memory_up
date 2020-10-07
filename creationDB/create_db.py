953477b179d12ae9fa152293c1353970db4963fd
kai00514 <onsen0514@gmail.com>
2020-10-07 22:02:29 +0900

    finish

--- creationDB/create_db.py
+++ creationDB/create_db.py
@@ -11,6 +11,8 @@
 import csv
 import itertools
 import pickle
+import time 
+import datetime
 
 from time import sleep
 
@@ -22,7 +24,12 @@ def setup_csv(file,thresh):
         for row in reader:
             result.append(row[thresh - 2:thresh])
         return result
-def show_question(lis):
+def get_time(t, cnv):
+    with open("log.txt","a") as f:
+        print("経過時間：{}, 日付：{}".format(str(t),cnv), file=f)
+    
+
+def show_question(lis, d):
 
     print("---------- Menu ----------")
     print("数字変換表暗記メニュー\n01~20 : type 0\n21~40 : type 1\n41~60 : type 2\n61~80 : type 3\n81~00 : type 4")
@@ -31,6 +38,9 @@ def show_question(lis):
     i = int(input("Please number of type : "))
     category = lis[0:][i]
     incorrect_list = []
+    now = time.ctime()
+    cnvtime = time.strftime(now)
+    t1 = time.time()
     for qes in category:
         ans = qes[1]
         num = qes[0]
@@ -39,7 +49,11 @@ def show_question(lis):
             print("正解です")
         else:
             print("不正解です")
-            incorrect_list.append(qes)
+        incorrect_list.append(qes)
+
+    t2 = time.time()
+    t = int(t2 - t1)
+    get_time(t,d)
     print("Process finish!")
     print("--------------------------")
     if len(incorrect_list) == 0:
@@ -49,16 +63,18 @@ def show_question(lis):
         for a in incorrect_list:
             print(a)
 
-
 if __name__ == "__main__":
-    
+
     f = "./datas/memories_up.csv"
     thresh_list = [2, 4, 6, 8, 10]
     lis = []
     for th in thresh_list:
         result_1 = setup_csv(f, th)
         lis.append(result_1[0:])
-    show_question(lis)
 
+    dt_now = datetime.datetime.now()
+    dt = dt_now.strftime('%Y年%m月%d日 %H:%M:%S')
+    d = dt[5:]
+    show_question(lis,d)
 
 
