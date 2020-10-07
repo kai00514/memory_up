5237813db3bb7b670958d48d2d178157168bc9b7
kai00514 <onsen0514@gmail.com>
2020-10-07 20:50:45 +0900

    code 2 function

--- creationDB/create_db.py
+++ creationDB/create_db.py
@@ -22,39 +22,8 @@ def setup_csv(file,thresh):
         for row in reader:
             result.append(row[thresh - 2:thresh])
         return result
+def show_question(lis):
 
-def list2dict(result):
-    
-    for _ in list(range(20)):
-        st_list = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39]
-        string_result = [result[r] for r in st_list]
-        row_st = [int(i) - 1 for i in st_list]
-        number_result = [result[r] for r in row_st]
-        dic = {key: val for key, val in zip(number_result, string_result)}
-        return dic
-    #dic_memories = list2dict(row)
-    #dic.update(dic_memories)
-    #pickle_dump(dic, "dictonary.pickle")
-    #print(pickle_load('dictonary.pickle'))
-
-def pickle_dump(obj, path):
-    with open(path, mode='wb') as f:
-        pickle.dump(obj,f)
-
-def pickle_load(path):
-    with open(path, mode='rb') as f:
-        data = pickle.load(f)
-        return data
-
-if __name__ == "__main__":
-    
-    f = "./datas/memories_up.csv"
-    thresh_list = [2, 4, 6, 8, 10]
-    lis = []
-    for th in thresh_list:
-        result_1 = setup_csv(f, th)
-        row = list(itertools.chain.from_iterable(result_1))
-        lis.append(result_1[0:])
     print("---------- Menu ----------")
     print("数字変換表暗記メニュー\n01~20 : type 0\n21~40 : type 1\n41~60 : type 2\n61~80 : type 3\n81~00 : type 4")
     print("--------------------------")
@@ -79,5 +48,17 @@ if __name__ == "__main__":
         print("Incorrect answer")
         for a in incorrect_list:
             print(a)
+
+
+if __name__ == "__main__":
+    
+    f = "./datas/memories_up.csv"
+    thresh_list = [2, 4, 6, 8, 10]
+    lis = []
+    for th in thresh_list:
+        result_1 = setup_csv(f, th)
+        lis.append(result_1[0:])
+    show_question(lis)
+
 
 
